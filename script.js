1ac860e52146196eb3310ad8df3e5fa3c9aa97e9
Rayan Ahmedy <rayanahmedy@Rayans-MacBook-Pro.local>
2020-09-06 14:08:58 -0400

    fixed application and bugs

--- script.js
+++ script.js
@@ -1,189 +1,63 @@
-// Assignment Code
-var enter;
-var options;
-var charU = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var charL = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var numC = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",];
-var specialChar = ["!", "#", "$", "%", "&", "'", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
-
-// function randomNumber(array) {
-//     return Math.floor(Math.random() * array.length);
-// }
-
-// function randomChoice(array) {
-//     return array[randomNumber(array)];
-// }
-
-// function mix(password) {
-//     var toss = password.split("");
-//     toss.sort(function() {
-//         return 0.5 - Math.random();
-//     });
-//     var returnP = mix.join("");
-//     return returnP;
-// }
-// Write password to the #password input
+let upperChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+let lowerChar = 'abcdefthijklmnopqrstuvwxyz';
+let specialChar = '!@#$%^&<>*_+~`|}{[]:;?,./-=';
+let numChar = '0123456789';
 
 
-function createPassword() {
-    var length = prompt("How many characters would you like your password to be?");
-    if (length < 8 || length > 128) {
+// Added  event listener to the button
+document.getElementById('generate').addEventListener('click', generatePassword);
 
-        alert("Length must be between 8 and 128 characters");
-        return;
-    }
-};
+function generatePassword() {
+	let passwordChar = '';
+	let userPassword = '';
 
-// createPassword ();
-    var lowers = confirm("Would you like to use lowercase letters?");
-    var uppers = confirm("Would you like to use Uppercase letters?");
-    var numbers = confirm("Would you like to use numbers?");
-    var symbols = confirm("Would you like to use special characters?");
+	// ask  user for character length
+	var passL = prompt(
+		'How many characters would you like your password to be? (please choose between 8 and 128 characters)'
+	).trim();
 
-if (uppers != true && lowers != true && numbers != true && symbols != true) {
+	// check if the input is between 8 and 128
+	if (passL >= 8 && passL <= 128) {
+        // Confirm choices
+        const numbers = confirm('Do you want numbers in the password ?');
+        const special = confirm('Do you want special characters in the password ?');
+        const uppercase = confirm('Do you want Uppercase letters in your password?');
+		const lowercase = confirm('Do you want lowercase letters in your pasword?');
+		
+		
 
-    alert("Please select at least on of the following character types");
+		// Concatenate strings if confirms are true
+		if (lowercase) {
+			passwordChar += lowerChar;
+		}
+		if (uppercase) {
+			passwordChar += upperChar;
+		}
+		if (numbers) {
+			passwordChar += numChar;
+		}
+		if (special) {
+			passwordChar += specialChar;
+		}
+
+		// feedback if not choices are selected 
+		if (!lowercase && !uppercase && !numbers && !special) {
+			alert('You must choose at least one option!!');
+			generatePassword();
+			userPassword = '';
+			return;
+		}
+
+		for (let i = 0; i < passL; i++) {
+			userPassword += passwordChar.charAt(Math.floor(Math.random() * passwordChar.length));
+		}
+
+		// Add the new password to the html
+		var yourPassword = document.getElementById('password');
+		yourPassword.textContent = userPassword.replace('<', '&lt').replace('>', '&gt');
+		document.getElementById('password').style.color = 'rgb(69, 69, 69)';
+	} else {
+		alert('You must choose a number between 8 and 128 characters!');
+		generatePassword();
+	}
 }
-var userInput = {
-    length: length,
-    lowers: lowers,
-    uppers: uppers,
-    numbers: numbers,
-    symbols: symbols,
-}
-return userInput;
-          
-// function randomNumber(arr) {
-//     var randomIndex = Math.floor(Math.random() * arr.length);
-//     var randomElement = arr[randomIndex]
-//     return randomElement;
-// }
-// randomNumber(charL);
-
-
-// createPassword()
-
-// function generatePassword() {
-//     var pass = '';
-//     var strng = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz0123456789@#$" + "0, 1, 2, 3, 4, 5, 6, 7, 8, 9";
-//     //   created a for loop to ensure when button is clicked, it generates a new password.
-//     for (i = 1; i <= 20; i++) {
-//         var char = Math.floor(Math.random() * strng.length + 1);
-
-//         pass += strng.charAt(char)
-//     }
-
-//     return pass + "";
-// };
-// var generateBtn = document.querySelector("#generate");
-
-// function writePassword() {
-//     var password = generatePassword();
-//     var passwordText = document.querySelector("#password");
-
-//     passwordText.value = password;
-
-// }
-
-// generateBtn.addEventListener("click");
-
-//     }
-//     else if (lowers && uppers && numbers && symbols) {
-//         options = charL.concat(charU, numC, specialChar);
-
-//         // password += randomChoice(charL);
-//         // confirmA.push(charL)
-//         // firstChar ++;
-//     }
-
-//     // 3
-//     else if (lowers && uppers && numbers) {
-//         options = charL.concat(charU, numC);
-
-//         // password += randomChoice(charU);
-//         // confirmA.push(charU)
-//         // firstChar ++;
-//     }
-//     else if (lowers && uppers && symbols) {
-//         options = charL.concat(charU, specialChar);
-
-//         // password += randomChoice(numC);
-//         // confirmA.push(numC)
-//         // firstChar ++;
-//     }
-//     else if (lowers && numbers && symbols) {
-//         options = charL.concat(numC, specialChar);
-
-//         // password += randomChoice(specialChar);
-//         // confirmA.push(specialChar)
-//         // firstChar ++;
-//     }
-//     else if (uppers && numbers && symbols) {
-//         options = charU.concat(numC, specialChar)
-
-//     }
-
-//     // 2
-//     else if (uppers && lowers) {
-//         options = charU.concat(charL);
-//     }
-//     else if (uppers && numbers) {
-//         options = charU.concat(numC);
-//     }
-//     else if (uppers && symbols) {
-//         options = charU.concat(specialChar);
-//     }
-//     else if (symbols && numbers) {
-//         options = specialChar.concat(numC);
-//     }
-//     else if (lowers && symbols) {
-//         options = charL.concat(specialChar);
-//     }
-//     else if (lowers && numbers) {
-//         options = charL.concat(numC);
-//     }
-
-//     // 1 
-//     else if (symbols) {
-//         options = specialChar;
-//     }
-//     else if (numbers) {
-//         options = numC;
-//     }
-//     else if (lowers) {
-//         options = charL;
-//     }
-//     else if (uppers) {
-//         options = charU;
-//     }
-
-
-//     var password = [];
-
-//     for (var i = 0; i < enter; i++) {
-//         var theOptions = options[Math.floor(Math.random() * options.length)];
-//         password.push(theOptions);
-//     }
-//     var ps = password.join("");
-//     UserInput(ps);
-//     return ps;
-// }
-// function UserInput(ps) {
-//     document.getElementById("#password").textContent = ps;
-
-// }
-
-// var generateBtn = document.querySelector("#generate");
-
-// function writePassword() {
-//     var password = createPassword();
-//     var passwordText = document.querySelector("#password");
-
-//     passwordText.value = password;
-// }
-
-
-
-
-
-
